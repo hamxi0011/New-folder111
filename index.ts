021f2e9e4793ac1086bcfd541c7f2a8587389034
hamxi011 <hamxishah1098@gmail.com>
2024-04-05 03:54:52 -0700

    jj

---
+++ index.ts
@@ -0,0 +1,101 @@
+// // let name1:string = "jadajsjda"
+// // console.log(name1)
+
+// // const name2:string = "djjdddasdk"
+// // console.log(name2)
+
+// // let n1:number = 1
+// // console.log(n1)
+
+// // lett message:string = "Hello World";//syntax error
+// // console.log(message);
+
+// // let message = "Hello World";
+// // console.log(message);
+
+// //strongly typed syntax
+// // let a : string = "ggsg"
+// // a = "USA";
+// // let b : number = 9;
+// // let c : boolean = true;
+
+// // //type inference
+// // let e = "USA";
+// // let f = 10.9;
+// // f = 22;
+// // let g = false;
+// // g = true;
+
+
+// //use const where variable values do not change
+// // const a:number = 5;
+// // const b : number = 33;
+// // const c:string ="best";
+
+// // //I suggest use let instead of var everywhere, 
+// // //becuase let has blocked scope
+// // if (true) {
+// // 	var z:number = 4;
+// //     console.log(a)
+// // 	//use z
+// // }
+// // else {
+// // 	let z = "string";
+// // 	//use z
+// // }
+// // console.log("let: " + z);// Error: z is not defined in this scope
+
+
+// // const name12:number[] = [1,2,3,4,5,6,3]
+// // console.log(name12[4])
+
+// // const name13:string[] = ["hhdh","jsjkjd","djdj"]
+// // console.log(name13[1])
+
+
+
+// function add()
+// {
+//     console.log("hi")
+// }
+
+// add()
+
+// function add1(x:string){
+//     return console.log(x)
+// }
+
+// add1("hgashahdg")
+
+// let myFunction = () => {
+//     return console.log("qqqwwee")
+// }
+
+// myFunction()
+
+// let my = (x:string) => {
+//     return console.log(x)
+// }
+// my("tststdst")
+
+let num1:number = 12
+let num2:number = 40
+
+if(num1<num2){
+    console.log("hi")
+}else{
+    console.log("bye");
+    
+}
+
+
+const person ={
+    name:"aass",
+    contact:234423343
+}
+console.log(person.contact)
+
+
+
+
+
